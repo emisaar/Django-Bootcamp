d196ed716c1ec69c7c00229b61226f0b37dcf4e3
emisaar <emisaar0406@gmail.com>
2023-03-05 18:00:55 -0600

    Fix settings

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import pymysql
+
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'videogames',  
-        'USER': 'root',  
+        'USER': 'emisaucedo',  
         'PASSWORD': 'bootcamp',  
         'HOST': 'localhost',  
         'PORT': '',
